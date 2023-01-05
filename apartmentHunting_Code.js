797df12c93b1cfa17b4afdeb96cfa861a27e3220
Rajkumar-Juvva <juvvarajkumar@gmail.com>
2023-01-05 13:15:32 +0530

    Update apartmentHunting_Code.js

--- apartmentHunting_Code.js
+++ apartmentHunting_Code.js
@@ -1,7 +1,7 @@
 function apartmentHunting(blocks, reqs) {
     const n = blocks.length;
 
-    //
+    //Initialize an array currentHavingRequirements to track whether each requirement is present at the current block.
     currentHavingRequirements = []
     for(let req of reqs){
         currentHavingRequirements.push(false);
@@ -12,20 +12,27 @@ function apartmentHunting(blocks, reqs) {
         }
     }
 
+    // Initialize an array presentReqs to store the requirements that are present in at least one block.
+    // Iterate through currentHavingRequirements and add the corresponding requirement to presentReqs if it is true.
     presentReqs = [];
     for(let j=0;j<reqs.length;j++){
         if(currentHavingRequirements[j]){
             presentReqs.push(reqs[j]);
         }
     }
+    // Set the value of reqs to be presentReqs. Here can eliminate some reqs.
     reqs = [...presentReqs];
     // console.log(reqs);
 
+    // Initialize an array optimalDistance to store the minimum distance from each requirement to the current block.
     optimalDistance = []
     for(let req of reqs){
         optimalDistance.push(Number.MAX_SAFE_INTEGER);
     }
 
+    // Initialize an array rightOptimalDistance to store the minimum distance from each requirement to each block, starting from the last block and going backwards.
+    // Iterate through the blocks list in reverse order. For each block, iterate through each requirement in "reqs". If the current requirement is present in the current block, set the corresponding element in "optimalDistance" to 0. Otherwise, increment it by 1.
+    // Append a copy of "optimalDistance" to "rightOptimalDistance".
     var rightOptimalDistance = []
     for(let i=blocks.length-1;i>=0;i--){
         for(let j=0;j<reqs.length;j++){
@@ -42,6 +49,10 @@ function apartmentHunting(blocks, reqs) {
         leftOptimalDistance.push(Number.MAX_SAFE_INTEGER);
     }
 
+    // Iterate through each block in the "blocks" list. For each block, iterate through each requirement in "reqs".
+    // If the current requirement is present in the current block, set the corresponding element in "leftOptimalDistance" to 0.
+    // Otherwise, increment it by 1
+
     for(let i=0;i<blocks.length;i++){
         for(let j=0;j<reqs.length;j++){
             leftOptimalDistance[j] =  blocks[i][reqs[j]]||false ? 0 : leftOptimalDistance[j]+1;
@@ -52,11 +63,14 @@ function apartmentHunting(blocks, reqs) {
             max_dist = Math.max(max_dist,Math.min(leftOptimalDistance[j],rightOptimalDistance[n-i-1][j]));
         }
         
+        // If "max_dist" is less than "optimalDistance", set "optimalIndex" to the current index and "optimalDistance" to "max_dist"
         if (max_dist < optimalDistance){
             optimalIndex = i;
             optimalDistance = max_dist;
         }
     }
+
+    // Return the "optimalIndex".
 
     return optimalIndex
 }
